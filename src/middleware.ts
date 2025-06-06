6e6660ddbbb1b62966974250fd7c3a602dfb6a56
Leo LeBlanc <lleblancleo@gmail.com>
2025-06-05 21:39:26 -0700

    consolidating logs, adding handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,25 +9,24 @@ const adjustFeatureFlags = (params: URLSearchParams) => {
     console.log(params.entries())
     console.log(params.entries().forEach)
     console.log('end enumerating logs...')
-    params.entries().forEach(([key, value]) => {
-        if (FEATURE_FLAGS[key]) {
-            if (value === 'true' || value === 'false') {
-                response.cookies.set(key, value, { httpOnly: false })
+    if (params.entries()) {
+        params.entries().forEach(([key, value]) => {
+            if (FEATURE_FLAGS[key]) {
+                if (value === 'true' || value === 'false') {
+                    response.cookies.set(key, value, { httpOnly: false })
+                }
             }
-        }
-    })
+        })
+    } else {
+        console.log('no params detected');
+    }
 
     return response;
 }
 
 export function middleware(request: NextRequest) {
     try {
-        console.log('extracting params...')
-        console.log(request)
-        console.log(request.nextUrl)
-        console.log(request.nextUrl.searchParams)
         const params = request.nextUrl.searchParams
-        console.log('done extracting params...')
         return adjustFeatureFlags(params)
     } catch (error) {
         console.error('Middleware ran into an error... ', error)

bfef9630fe1174fcc1641c58ac964da3c5c88542
Leo LeBlanc <lleblancleo@gmail.com>
2025-06-05 21:04:16 -0700

    so... the existence of middleware is breaking my application?

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,3 @@ import { NextResponse } from 'next/server'
 export function middleware() {
     return NextResponse;
 }
-
-// Match only on page requests
-export const config = {
-    matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)'
-}

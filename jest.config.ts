db3e9577699a8a6e5ceb4a3835dcf7292b7f5934
Leo LeBlanc <lleblancleo@gmail.com>
2025-07-10 17:44:39 -0700

    adjusting jest config file

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,20 @@
 // found this online after trying to add babel, which...
-// messed up a lot of things. So, removing babel, adding this.
+// messed up a lot of things. So, removing babel, adjusting this.
 // I only need babel for the tests, so this seems to work
-// how exactly does it know what babel is if it's not installed...
-// who knows?
-// found here: https://github.com/agcty/nextjs-advanced-starter/issues/28
+// jest config docs: https://jestjs.io/docs/configuration
+// found this after attempting to add babel: https://github.com/agcty/nextjs-advanced-starter/issues/28
 
-module.exports = {
+import type { Config } from 'jest';
+
+const config: Config = {
     collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
     testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
     transform: {
         // Use babel-jest to transpile tests with the next/babel preset
+        // this comes with jest out of the box
         '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
     },
     transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
-};
+}
+
+export default config;
